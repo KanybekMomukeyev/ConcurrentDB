82eb85b8024e13ef243547e4f2488b4c4694550e
Kanybek <kano.momuke@gmail.com>
2017-05-08 17:57:41 +0600

    added example of code

---
+++ database/dbManager.go
@@ -0,0 +1,146 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	"log"
+)
+
+type Person struct {
+	FirstName string `db:"first_name"`
+	LastName  string `db:"last_name"`
+	Email     string
+}
+
+type Place struct {
+	Country string
+	City    string
+	TelCode int
+}
+
+type someInterface interface {
+	followChannel()
+	CreateSchema()
+	CreatePerson(per Person) error
+	CreatePlace(pl Place) error
+	GetAllPeople() ([]*Person, error)
+	GetAllPlaces() ([]*Place, error)
+}
+
+type dbManager struct {
+	DB *sqlx.DB
+	dbChannel chan func()
+}
+
+func NewDbManager() *dbManager {
+
+	db, err := sqlx.Connect("postgres", "dbname=template1 host=localhost sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	dbM := new(dbManager)
+	dbM.DB = db
+	dbM.dbChannel = make(chan func(), 1000)
+	go dbM.followChannel()
+
+	return dbM
+}
+
+func (dbM *dbManager) followChannel() {
+	for f := range dbM.dbChannel {
+		f()
+	}
+}
+
+func (dbM *dbManager) CreateSchema() {
+
+	f := func() {
+
+		var dropPerson = `
+		DROP TABLE IF EXISTS person;
+		`
+		var dropPlace = `
+		DROP TABLE IF EXISTS place;
+		`
+
+		dbM.DB.MustExec(dropPerson)
+		dbM.DB.MustExec(dropPlace)
+
+		var schema = `
+		CREATE TABLE IF NOT EXISTS person (
+    			first_name text,
+    			last_name text,
+    			email text
+		);
+
+		CREATE TABLE IF NOT EXISTS place (
+    			country text,
+    			city text,
+    			telcode integer
+		)`
+		dbM.DB.MustExec(schema)
+	}
+
+	dbM.dbChannel <- f
+}
+
+func (dbM *dbManager) CreatePerson(per Person) error {
+	f := func() {
+		tx := dbM.DB.MustBegin()
+		tx.MustExec("INSERT INTO person (first_name, last_name, email) VALUES ($1, $2, $3)", per.FirstName, per.LastName, per.Email)
+		tx.Commit()
+	}
+	dbM.dbChannel <- f
+	return nil
+}
+
+func (dbM *dbManager) CreatePlace(pl Place) error {
+	f := func() {
+		tx := dbM.DB.MustBegin()
+		tx.MustExec("INSERT INTO place (country, city, telcode) VALUES ($1, $2, $3)", pl.Country, pl.City, pl.TelCode)
+		tx.Commit()
+	}
+	dbM.dbChannel <- f
+	return nil
+}
+
+func (dbM *dbManager) GetAllPeople() ([]*Person, error) {
+
+	rows, err := dbM.DB.Queryx("SELECT first_name, last_name, email FROM person ORDER BY first_name ASC")
+	if err != nil {
+		print("error")
+	}
+
+	people := make([]*Person, 0)
+	for rows.Next() {
+		person := new(Person)
+		err := rows.Scan(&person.FirstName, &person.LastName, &person.Email)
+		if err != nil {
+			return nil, err
+		}
+		people = append(people, person)
+	}
+
+	return people, nil
+}
+
+func (dbM *dbManager) GetAllPlaces() ([]*Place, error) {
+
+	rows, err := dbM.DB.Queryx("SELECT country, city, telcode FROM place ORDER BY country ASC")
+	if err != nil {
+		print("error")
+	}
+
+	places := make([]*Place, 0)
+	for rows.Next() {
+		place := new(Place)
+		err := rows.Scan(&place.Country, &place.City, &place.TelCode)
+		if err != nil {
+			return nil, err
+		}
+		places = append(places, place)
+	}
+
+	return places, nil
+}
+
--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
-	db "github.com/KanybekMomukeyev/ConcurrentDB/database"
+	database "github.com/KanybekMomukeyev/ConcurrentDB/database"
 	"fmt"
+	"time"
 )
 
 func main() {
-	db.SomeDatabaseFunction()
-	fmt.Print("Hello world")
+
+	dbManager := database.NewDbManager()
+
+	dbManager.CreateSchema()
+
+	go dbManager.CreatePerson(database.Person{"first1","last1","email1"})
+	go dbManager.CreatePerson(database.Person{"first2","last2","email2"})
+	go dbManager.CreatePerson(database.Person{"first3","last3","email3"})
+	go dbManager.CreatePerson(database.Person{"first4","last4","email4"})
+	go dbManager.CreatePerson(database.Person{"first5","last5","email5"})
+	go dbManager.CreatePerson(database.Person{"first6","last6","email6"})
+
+	time.Sleep(2 * 1e9)
+
+	go dbManager.CreatePlace(database.Place{"country1","city1",1})
+	go dbManager.CreatePlace(database.Place{"country2","city2",2})
+	go dbManager.CreatePlace(database.Place{"country3","city3",3})
+
+	time.Sleep(2 * 1e9)
+
+	people, _ := dbManager.GetAllPeople()
+	for _, person := range people {
+		fmt.Println(person)
+	}
+
+	places, _ := dbManager.GetAllPlaces()
+	for _, place := range places {
+		fmt.Println(place)
+	}
 }
+
