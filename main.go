0415b7b63ce671b13d3641bb40e7eec1a0b55070
Kanybek <kano.momuke@gmail.com>
2017-05-08 12:57:20 +0600

    sdfdsf

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	db "github.com/KanybekMomukeyev/ConcurrentDB/database"
+	"fmt"
+)
 
 func main() {
+	db.SomeDatabaseFunction()
 	fmt.Print("Hello world")
 }
