574584a77defadf9eeac0fb392cbf14a10b8b618
Kanybek <kano.momuke@gmail.com>
2017-05-09 16:17:24 +0600

    aditional examples.

--- goroutine_examples/select_ex1.go
+++ goroutine_examples/select_ex1.go
@@ -3,16 +3,16 @@ import "time"
 import "fmt"
 func main() {
 
-	c1 := make(chan string)
-	c2 := make(chan string)
+	c1 := make(chan string, 1000)
+	c2 := make(chan string, 1000)
 
 	go func() {
-		time.Sleep(time.Second * 6)
+		time.Sleep(time.Second * 7)
 		c1 <- "one"
 	}()
 
 	go func() {
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 6)
 		c2 <- "two"
 	}()
 
@@ -22,4 +22,18 @@ func main() {
 	case msg2 := <-c2:
 		fmt.Println("received", msg2)
 	}
+
+	//go func() {
+	//	for {
+	//		select {
+	//		case msg1 := <-c1:
+	//			fmt.Println("received", msg1)
+	//		case msg2 := <-c2:
+	//			fmt.Println("received", msg2)
+	//		}
+	//	}
+	//}()
+	//
+	//var input string
+	//fmt.Scanln(&input)
 }
