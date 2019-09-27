11013519f348f3ac6442a4882e6ebb96861fcaee
kanybek <kano.momuke@gmail.com>
2019-09-27 11:58:11 +0600

    fix

--- goroutine_examples/select_ex2.go
+++ goroutine_examples/select_ex2.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func main() {
+func lalal() {
 	c1 := make(chan string, 1)
 	c2 := make(chan string, 1)
 
@@ -14,25 +14,25 @@ func main() {
 			c1 <- "from 1"
 			time.Sleep(time.Second * 2)
 		}
-	} ()
+	}()
 	go func() {
 		for {
 			c2 <- "from 2"
 			time.Sleep(time.Second * 3)
 		}
-	} ()
+	}()
 	go func() {
 		for {
 			select {
-			case msg1 := <- c1:
+			case msg1 := <-c1:
 				fmt.Println("Message 1", msg1)
-			case msg2 := <- c2:
+			case msg2 := <-c2:
 				fmt.Println("Message 2", msg2)
-			case <- time.After(time.Second):
+			case <-time.After(time.Second):
 				fmt.Println("timeout")
 			}
 		}
-	} ()
+	}()
 
 	//var input string
 	//fmt.Scanln(&input)
